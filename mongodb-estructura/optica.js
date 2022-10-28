e7fe6cfbc0561e0ad2325598ef9fb9ee69686cb1
Gabriel Valiente <gsvalient@gmail.com>
2022-10-28 09:43:57 +0200

    modified clientes data

--- mongodb-estructura/optica.js
+++ mongodb-estructura/optica.js
@@ -331,3 +331,113 @@ db.gafas.insertMany(
     }
   ]
 )
+
+db.clientes.insertMany(
+  [{
+  "nombre": "Blane Pudan",
+  "codigo_postal": 33790,
+  "telefono": "390-482-1392",
+  "correo": "bpudan0@blogtalkradio.com",
+  "fecha_registro": "27/04/2021",
+  "recomendado": ""
+}, {
+  "nombre": "Shelley Shevelin",
+  "codigo_postal": 18548,
+  "telefono": "547-164-8385",
+  "correo": "sshevelin1@ed.gov",
+  "fecha_registro": "05/01/2022",
+  "recomendado": ""
+}, {
+  "nombre": "Phaedra Mathie",
+  "codigo_postal": 22235,
+  "telefono": "526-987-9650",
+  "correo": "pmathie2@last.fm",
+  "fecha_registro": "11/04/2022",
+  "recomendado": "ObjectID('635b8067602e696cb5b897a6')"
+}, {
+  "nombre": "Koo Brimley",
+  "codigo_postal": 42154,
+  "telefono": "215-284-0836",
+  "correo": "kbrimley3@irs.gov",
+  "fecha_registro": "06/04/2021",
+  "recomendado": ""
+}, {
+  "nombre": "Nalani Lampitt",
+  "codigo_postal": 41611,
+  "telefono": "542-856-6055",
+  "correo": "nlampitt4@storify.com",
+  "fecha_registro": "27/11/2021",
+  "recomendado": ""
+}, {
+  "nombre": "Kessia Petrolli",
+  "codigo_postal": 35037,
+  "telefono": "451-416-7917",
+  "correo": "kpetrolli5@sphinn.com",
+  "fecha_registro": "12/01/2021",
+  "recomendado": "ObjectId('635b8067602e696cb5b897a6')"
+}, {
+  "nombre": "Dickie Sweed",
+  "codigo_postal": 41417,
+  "telefono": "590-359-9515",
+  "correo": "dsweed6@newsvine.com",
+  "fecha_registro": "05/10/2021",
+  "recomendado": ""
+}, {
+  "nombre": "Jacynth Sizeland",
+  "codigo_postal": 85531,
+  "telefono": "356-396-0305",
+  "correo": "jsizeland7@lulu.com",
+  "fecha_registro": "30/07/2022",
+  "recomendado": "ObjectId('635b8067602e696cb5b897ac')"
+}, {
+  "nombre": "Brinna Crooke",
+  "codigo_postal": 19889,
+  "telefono": "104-861-0069",
+  "correo": "bcrooke8@apache.org",
+  "fecha_registro": "26/04/2021",
+  "recomendado": ""
+}, {
+  "nombre": "Gael Cline",
+  "codigo_postal": 95985,
+  "telefono": "802-897-3846",
+  "correo": "gcline9@blogspot.com",
+  "fecha_registro": "17/12/2021",
+  "recomendado": ""
+}, {
+  "nombre": "Agathe Girdwood",
+  "codigo_postal": 93745,
+  "telefono": "657-350-5171",
+  "correo": "agirdwooda@weibo.com",
+  "fecha_registro": "27/11/2021",
+  "recomendado": ""
+}, {
+  "nombre": "Karol Moulding",
+  "codigo_postal": 38535,
+  "telefono": "582-309-3752",
+  "correo": "kmouldingb@cnet.com",
+  "fecha_registro": "12/12/2022",
+  "recomendado": ""
+}, {
+  "nombre": "Florence Rawsthorne",
+  "codigo_postal": 78824,
+  "telefono": "299-185-0132",
+  "correo": "frawsthornec@cyberchimps.com",
+  "fecha_registro": "29/07/2021",
+  "recomendado": "ObjectId('635b8067602e696cb5b897af')"
+}, {
+  "nombre": "Reine McKernon",
+  "codigo_postal": 56856,
+  "telefono": "514-693-0840",
+  "correo": "rmckernond@typepad.com",
+  "fecha_registro": "05/10/2022",
+  "recomendado": ""
+}, {
+  "nombre": "Marigold Sutherel",
+  "codigo_postal": 73878,
+  "telefono": "886-958-2095",
+  "correo": "msutherele@facebook.com",
+  "fecha_registro": "15/12/2022",
+  "recomendado": "ObjectId('635b8067602e696cb5b897b3')"
+}]
+)
+
